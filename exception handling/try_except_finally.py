e426f10b12eada7034351fe2fd784cf40e98d9e7
unknown <bhuwanpulamimagar15220@gmail.com>
2026-04-20 09:04:57 +0545

    initial commit

---
+++ exception handling/try_except_finally.py
@@ -0,0 +1,41 @@
+# try:
+#     a = int(input("Enter a number: "))
+#     b = int(input("Enter a number: "))
+#     result = a/b
+
+# except ZeroDivisionError:
+#     print("cannot divide by zero")
+
+# #else always runs if try block will run
+# else:
+#     print(f"Result = {result}")
+
+# #finally always runs if there is exception or not
+# finally:
+#     print("This is always executed")
+
+
+#classwork
+# create a function that takes any kind of iterable as input
+# and try to reverse it
+# use try-except-else-finally block to handle the case where the input is not an iterable
+
+def rev_num(iterable):
+    try:
+        result = iterable[::-1]
+    
+    except TypeError:
+        error_type = "type error"
+        message = "invalid input"
+
+    else:
+        print(f"successfully reversed {result}")
+
+    finally:
+        if error_type:
+            return f'{error_type} : {message}'
+        else:
+            return result
+
+iterable = input("Enter a number: ")
+print(rev_num(iterable))
