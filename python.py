334c00266c297ce9d3d488dd9e1cb83d8d0cfc6b
unknown <bhuwanpulamimagar15220@gmail.com>
2026-05-07 18:05:58 +0545

    Updated

---
+++ python.py
@@ -0,0 +1,26 @@
+def blackjack_hand_greater_than(hand_1, hand_2):
+    def hand_value(hand):
+        value = 0
+        aces = 0
+        
+        for card in hand:
+            if card in ['J', 'Q', 'K']:
+                value += 10
+            elif card == 'A':
+                aces += 1
+            else:
+                value += int(card)
+        
+        # Add aces as 11 where possible, otherwise 1
+        for _ in range(aces):
+            if value + 11 <= 21:
+                value += 11
+            else:
+                value += 1
+        
+        return value
+
+    h1 = hand_value(hand_1)
+    h2 = hand_value(hand_2)
+
+    return h1 <= 21 and (h1 > h2 or h2 > 21)
