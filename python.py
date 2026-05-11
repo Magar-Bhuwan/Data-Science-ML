4037d43ba4efc688c8628ef73acec026cfd2d952
unknown <bhuwanpulamimagar15220@gmail.com>
2026-05-11 08:55:35 +0545

    Updated

--- python.py
+++ python.py
@@ -1,28 +1,10 @@
-def blackjack_hand_greater_than(hand_1, hand_2):
-    def hand_value(hand):
-        value = 0
-        aces = 0
-        
-        for card in hand:
-            if card in ['J', 'Q', 'K']:
-                value += 10
-            elif card == 'A':
-                aces += 1
-            else:
-                value += int(card)
-        
-        # Add aces as 11 where possible, otherwise 1
-        for _ in range(aces):
-            if value + 11 <= 21:
-                value += 11
-            else:
-                value += 1
-        
-        return value
+import numpy as np
 
-    h1 = hand_value(hand_1)
-    h2 = hand_value(hand_2)
 
-    return h1 <= 21 and (h1 > h2 or h2 > 21)
-
-help(range)
+data = pd.DataFrame({
+    "id":[1,3,5,7,9],
+    "name":["John","Jane","Jennifer","Johan","Jeff"],
+    "age":[25,-32,19,42,"55"],
+    "gender" : ["M","M","Z","F","F"],
+    "income":[400, 700, 300, np.nan, 600]
+})
